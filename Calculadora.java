15b3627b767b6d7586eedd8282206e28ec09dece
yairoliva <yairolivaIT@gmail.com>
2020-04-20 00:37:28 -0300

    Cambio de posiciones

--- Calculadora.java
+++ Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
 		Scanner teclado = new Scanner(System.in);
 		do {
 			System.out.println(
-					"Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
+					"Ingrese su operación \n 1 para multiplicar \n 2 para restar \n 3 para sumar \n 4 para dividir");
 			opcion = teclado.nextInt();
 		} while (opcion < 1 && opcion > 4);
 		System.out.println("Ingrese el primer numero");
@@ -19,16 +19,20 @@ public class Calculadora {
 		
 		switch(opcion) {
 		case 1:
-			resultado = a + b;
+			resultado = a * b;
 			break;
 		case 2:
 			resultado = a - b;
 			break;
 		case 3: 
-			resultado = a * b;
+			resultado = a + b;
 			break;
 		case 4:
 			resultado = a / b;
+			break;
+		
+		default:
+			System.out.println("Opción Inválida");
 			break;
 		}
 
